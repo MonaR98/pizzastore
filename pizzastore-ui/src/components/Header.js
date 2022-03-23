cf63da97b19b0bae14d4b14779b9c9075922ed23
MonaR98 <74354950+MonaR98@users.noreply.github.com>
2022-03-24 02:16:54 +0530

    Implemented static MakePizzaUI

--- pizzastore-ui/src/components/Header.js
+++ pizzastore-ui/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
-// import Logo from '../assets/pizza.png';
+import Logo from '../assets/pizzalogo.jpg';
+import './Header.css';
 
 export default function Header(){
     return (
-    <div className="container" style={{background:"red", color:"white"}}>
-        Make Your Pizza
+    <div className="header-container">
+        <div className="header-container-content">
+            <img className="logo" src={Logo} alt="" height="52px" width="52px" />
+            <div className="header-title">Make Your Pizza</div>
+        </div>
     </div>
     );
 }
--- pizzastore-ui/src/components/MakePizza.js
+++ pizzastore-ui/src/components/MakePizza.js
@@ -1,7 +1,70 @@
 import React from "react";
-
+import Pizza from '../assets/pizza.jpg';
+import './MakePizza.css';
+import { useNavigate } from 'react-router-dom';
 export default function MakePizza(){
+
+    const navigate=useNavigate();
     return (
-        <div>Choose your favourite ingredients</div>
+    <div className="container">
+        <div className="img-container">
+            <img className="pizza-img"src={Pizza} alt="" />
+        </div>
+        <div className="ingredients-container">
+            Choose Your Favourite Ingredients
+            <br />
+            <table>
+                <tr>
+                    <th>Ingredient</th>
+                    <th>Price</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>Cheese</td>
+                    <td>₹50</td>
+                    <td> <input type="checkbox" /></td>
+                </tr>
+                <tr>
+                    <td>Onion</td>
+                    <td>₹50</td>
+                    <td> <input type="checkbox" /></td>
+                </tr>
+                <tr>
+                    <td>Tomato</td>
+                    <td>₹50</td>
+                    <td> <input type="checkbox" /></td>
+                </tr>
+                <tr>
+                    <td>Capsicum</td>
+                    <td>₹50</td>
+                    <td> <input type="checkbox" /></td>
+                </tr>
+                <tr>
+                    <td>Corn</td>
+                    <td>₹50</td>
+                    <td> <input type="checkbox" /></td>
+                </tr>
+                <tr>
+                    <td>Mushrooms</td>
+                    <td>₹50</td>
+                    <td> <input type="checkbox" /></td>
+                </tr>
+                <tr>
+                    <td>Olives</td>
+                    <td>₹50</td>
+                    <td> <input type="checkbox" /></td>
+                </tr>
+            
+            </table>
+            <br />
+            <button id="bake-btn"
+          onClick={() => navigate("/cart")}
+          className="proceed"
+        >
+          Ready to Bake?
+        </button>
+        </div>
+        
+    </div>
     )
 }
